0a8c954dba75acaf310dac7b4575f5c06d42c2f3
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-06-19 15:22:28 -0400

    Finished Exercise 2

---
+++ MethodsExercise/Functions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class Functions
+    {
+        public int Add(int num1, int num2)
+        {
+            int add = num1 + num2;
+            return add;
+        }
+        public int Subtract(int num1, int num2)
+        {
+            int sub = num1 - num2;
+            return sub;
+        }
+        public int Multiply(int num1, int num2)
+        {
+            int mult = num1 * num2;
+            return mult;
+        }
+        public int Divide(int num1, int num2)
+        {
+            int div = num1 / num2;
+            return div;
+        }
+        public int Modulus(int num1, int num2)
+        {
+            int mod = num1 % num2;
+            return mod;
+        }
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Add(1, 3));
+            Console.WriteLine(Subtract(13, 3));
+            Console.WriteLine(Multiply(2, 3));
+            Console.WriteLine(Divide(9, 3));
+            Console.WriteLine(Modulus(8, 3));
+
             Console.WriteLine("What is your name?");
             string? name = Console.ReadLine();
 
@@ -19,5 +25,26 @@
             Console.WriteLine($"Hello, my name is {name} and my favorite color is {color} I like listening to {band} with my {animal}.");
 
         }
+        public static int Add(int num1, int num2)
+        { 
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
     }
 }
