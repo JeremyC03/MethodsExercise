e571018473038d1585269ac903ab7271f31c9833
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-06-19 12:23:00 -0400

    Finished Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string? color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string? animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string? band = Console.ReadLine();
+
+            Console.WriteLine($"Hello, my name is {name} and my favorite color is {color} I like listening to {band} with my {animal}.");
+
         }
     }
 }
